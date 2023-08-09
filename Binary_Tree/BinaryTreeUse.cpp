dbfa75bd8392441951635ecbea127d7f868bbfb0
Tushar Gupta <tushargupta4870@gmail.com>
2023-08-09 21:54:26 +0530

    level wise input added

--- Binary_Tree/BinaryTreeUse.cpp
+++ Binary_Tree/BinaryTreeUse.cpp
@@ -61,6 +61,45 @@ BinaryTreeNode<int> *takeInput()
 		3:
 	*/
 }
+BinaryTreeNode<int> *takeInputLevelWise()
+{
+	int rootData;
+	cout << "Enter root data" << endl;
+	cin >> rootData;
+	if (rootData == -1)
+	{
+		return NULL;
+	}
+
+	BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+
+	queue<BinaryTreeNode<int> *> pendingNodes;
+	pendingNodes.push(root);
+	while (pendingNodes.size() != 0)
+	{
+		BinaryTreeNode<int> *front = pendingNodes.front();
+		pendingNodes.pop();
+		cout << "Enter left child of " << front->data << endl;
+		int leftChildData;
+		cin >> leftChildData;
+		if (leftChildData != -1)
+		{
+			BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChildData);
+			front->left = child;
+			pendingNodes.push(child);
+		}
+		cout << "Enter right child of " << front->data << endl;
+		int rightChildData;
+		cin >> rightChildData;
+		if (rightChildData != -1)
+		{
+			BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChildData);
+			front->right = child;
+			pendingNodes.push(child);
+		}
+	}
+	return root;
+}
 int main()
 {
 	/*BinaryTreeNode<int>* root = new BinaryTreeNode<int>(1);
